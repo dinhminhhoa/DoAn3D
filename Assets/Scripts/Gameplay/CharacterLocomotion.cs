d7b44dba267b9eff068f8e334f7168f62aee67e1
dinhminhhoa <dinhminhhoa25061999@gmail.com>
2023-07-16 19:52:58 +0700

    [add] .

---
+++ Assets/Scripts/Gameplay/CharacterLocomotion.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLocomotion : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterController characterController;
+    private Vector2 userInput;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        userInput.x = Input.GetAxis("Horizontal");
+        userInput.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("InputX", userInput.x);
+        animator.SetFloat("InputY", userInput.y);
+
+
+
+    }
+}
