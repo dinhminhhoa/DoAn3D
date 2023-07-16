29108c8ef069edf14457c99e340fd85f30b8e5e1
dinhminhhoa <dinhminhhoa25061999@gmail.com>
2023-07-16 19:51:10 +0700

    [add] .

---
+++ Assets/Scripts/Gameplay/CrossHairTarget.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHairTarget : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Ray ray;
+    private RaycastHit hitInfo;
+    private float maxCrossHairTargetDistance;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Start()
+    {
+        if(DataManager.HasInstance)
+        {
+            maxCrossHairTargetDistance = DataManager.Instance.GlobalConfig.maxCroissHairTargetDistance;
+        }
+    }
+
+    void Update()
+    {
+        ray.origin = mainCamera.transform.position;
+        ray.direction = mainCamera.transform.forward;
+
+        if( Physics.Raycast(ray, out hitInfo) ) 
+        {
+            transform.position = hitInfo.point;
+        }
+        else
+        {
+            transform.position = ray.GetPoint(maxCrossHairTargetDistance);
+        }
+    }
+}
